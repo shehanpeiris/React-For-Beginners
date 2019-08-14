6eca204dcb15363d5149733d6577fe4e5f10b02b
shehanpeiris <shehanpeiris@gmail.com>
2019-08-13 23:11:04 -0500

    built StorePicker component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+// Import Dependencies
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "./components/StorePicker";
+
+render(<StorePicker />, document.querySelector("#main"));
