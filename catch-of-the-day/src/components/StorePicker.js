a3c2d34eea9e4598de5ea9948a424041935c0c5b
shehanpeiris <shehanpeiris@gmail.com>
2019-08-24 17:38:56 -0500

    added routing functionality to storepicker form

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
-class StorePicker extends React.Component {
+class StorePicker extends React.Component { 
+  storeInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.storeInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" ref={this.storeInput} required placeholder="Store Name" defaultValue={getFunName()}/>
         <button type="submit">Visit Store</button>
       </form>
     );
